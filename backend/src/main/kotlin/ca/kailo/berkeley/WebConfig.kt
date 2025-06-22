cc5966dd8fa7c6eb4b91156f8a9a451beb72f42a
klokailo <klo.kai.lo@gmail.com>
2025-06-21 23:24:48 -0700

    Added web config to allow cors policy

---
+++ backend/src/main/kotlin/ca/kailo/berkeley/WebConfig.kt
@@ -0,0 +1,15 @@
+package ca.kailo.berkeley
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://100.69.248.75:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+    }
+}
